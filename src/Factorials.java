bb2324b38e7289adf1e2daedb8077c5e873b294c
Никита <grand.marchello@gmail.com>
2021-04-02 14:39:42 +0400

    Решение задания 2

--- src/Factorials.java
+++ src/Factorials.java
@@ -2,21 +2,33 @@ import java.util.Scanner;
 
 public class Factorials {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IllegalArgumentException {
         String keepGoing = "Y";
         Scanner scan = new Scanner(System.in);
 
         while (keepGoing.equals("Y")) {
-            System.out.print("Введите число: ");
-            int val = scan.nextInt();
-            System.out.println(
-                    String.format(
-                            "Факториал(\"%d\") = %d",
-                            val, MathUtils.factorial(val)
-                    )
-            );
-            System.out.print("Вычислить другой факториал? (y/n) ");
-            keepGoing = scan.next().toUpperCase();
+            try {
+                System.out.print("Введите число: ");
+                int val = scan.nextInt();
+                if (val < 1)
+                    throw new IllegalArgumentException("Меньше");
+                else if (val > 16)
+                    throw new IllegalArgumentException("Больше");
+                System.out.println(
+                        String.format(
+                                "Факториал(\"%d\") = %d",
+                                val, MathUtils.factorial(val)
+                        )
+                );
+                System.out.print("Вычислить другой факториал? (y/n) ");
+                keepGoing = scan.next().toUpperCase();
+            } catch (IllegalArgumentException e) {
+                if (e.getMessage().equals("Меньше")) {
+                    System.out.println("Ошибка!\nВведенное число меньше 1!\n");
+                } else if (e.getMessage().equals("Больше")) {
+                    System.out.println("Ошибка!\nВведенное число больше 16!\n");
+                }
+            }
         }
     }
 }
